c7febba3c8497d5b99490afe31cfaf72ebf5f09a
Joao Victor Costa <victor_ngl2@hotmail.com>
2023-04-17 11:55:14 -0300

    add favicon e logo

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -4,8 +4,8 @@ function Footer() {
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Eficaz Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Eficaz</span>
+                        <img src="/logo.png" className="h-12 mr-3" alt="Eficaz Logo" />
+                        {/*<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Eficaz</span>*/}
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                         <li>
--- components/utils/Navbar.tsx
+++ components/utils/Navbar.tsx
@@ -13,8 +13,8 @@ function Navbar() {
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://flowbite.com/" className="flex items-center">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Eficaz</span>
+            <img src='/logo.png' className="h-12 mr-3" alt="Flowbite Logo" />
+            {/*<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Eficaz</span>*/}
           </a>
           <div className="flex items-center md:order-2">
             <button type="button"
